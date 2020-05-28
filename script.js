182903409ca060a4fd3f62923e1913c38835d1a2
victorsc45 <victorlitzau@gmail.com>
2020-05-28 17:53:14 -0400

    css style good and js file for loop started

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+  //possible password values
+  let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxzy1234567890!@#$%^&*()-_=";
+  passwordText.values = password;
+ // create loop to create password
+  for(let i=0; i <= 30; i++) {
+    generatePassword.values[i].charAt(math.floor(math.random() * math.floor(values.length -1)));
+  }
 }
 
 // Add event listener to generate button
