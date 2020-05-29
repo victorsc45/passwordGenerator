8146aadc23324cb43aadf7fd29edcb503c487f61
victorsc45 <victorlitzau@gmail.com>
2020-05-29 12:52:56 -0400

    javascript if else for password type selector

--- script.js
+++ script.js
@@ -1,30 +1,51 @@
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
+disable[generateBtn];
 
 // Write password to the #password input
 function writePassword() {
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
-  //possible password values
-  let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxzy1234567890!@#$%^&*()-_=";
-  passwordText.values = password;
- // create loop to create password
-  for(let i=0; i <= 30; i++) {
-    generatePassword.values[i].charAt(math.floor(math.random() * math.floor(values.length -1)));
-  }
+  
+  
+  if(document.querySelector("#uppers")){
+   let passType = upperValues;
+   enable[generateBtn];
+ }
+ else if(document.querySelector("#lowers")){
+   let passType = lowerValues;
+   enable[generateBtn];
+ }
+ else if(document.querySelector("#nums")){
+   let passType = numValues;
+   enable[generateBtn];
+ }
+ else  {
+   let passType = strongValues;
+   enable[generateBtn];
+ }
 }
+  //possible password values
+  let strongValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxzy1234567890!@#$%^&*()-_=";
+  let numValues = "1234567890";
+  let lowerValues = "abcdefghijklmnopqrstuvwyxz";
+  let upperValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  passwordText.values = password;
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-// example simple look here !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-{/* <script type="text/javascript">
-// Loop 10 times
-for (var i = 0; i < 10; i++) {
-  // Generate a random number between 1 and 10
-  // Math.floor will round down, meaning we would get a number between 0 and 9, so we'll always add 1 to bump it up.
-  var num = Math.floor(Math.random() * 10) + 1;
 
-  // Display in console
-  console.log(num);
-}
-</script> */}
+// Assignment Code original
+//var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+//function writePassword(uppers, lowers, nums, strong) {
+  //var password = generatePassword();
+ // var passwordText = document.querySelector("#password");
+
+ // passwordText.value = password;
+
+//}
+
+// Add event listener to generate button
+//generateBtn.addEventListener("click", writePassword);
